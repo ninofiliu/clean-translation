1b03c172b033854760ff25cd312ee863c05977bb
ninofiliu <nino.filiu@gmail.com>
2020-04-12 01:11:33 +0200

    lang ui

--- lang-api/main.js
+++ lang-api/main.js
@@ -1,26 +1,32 @@
 const fs = require('fs');
 const http = require('http');
 
-const messagePath = `${__dirname}/messages.json`
+const messagePath = `${__dirname}/messages.json`;
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Allow-Headers': '*',
+};
 
 const server = http.createServer((req, res) => {
   switch(req.method) {
     case 'GET':
       const rs = fs.createReadStream(messagePath);
-      res.writeHead(200, {
-        'Access-Control-Allow-Origin': '*',
-      });
+      res.writeHead(200, corsHeaders);
       rs.pipe(res);
       break;
     case 'PUT':
       const ws = fs.createWriteStream(messagePath);
       req.pipe(ws);
-      req.on('end', () => {
-        res.writeHead(200, {
-          'Access-Control-Allow-Origin': '*',
-        });
-        res.end();
+      res.writeHead(200, corsHeaders);
+      res.end();
+      break;
+    case 'OPTIONS':
+      res.writeHead(200, {
+        ...corsHeaders,
+        'Allow': 'OPTIONS, GET, PUT',
       });
+      res.end();
       break;
   }
 });
--- lang-ui/src/App.vue
+++ lang-ui/src/App.vue
@@ -1,3 +1,45 @@
 <template>
-  <div/>
+  <div>
+    <div v-if="! messages">Loading...</div>
+    <div v-else>
+      <button @click="save">Save</button>
+      <ul>
+        <li v-for="[locale, data] of Object.entries(messages)" :key="locale">
+          {{ locale }}
+          <ul>
+            <li v-for="[key, value] of Object.entries(data)" :key="key">
+              <span>{{ key }}</span>
+              <input :value="value" @input="(evt) => update(locale, key, evt.target.value)">
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<script>
+const langApiOrigin = 'http://localhost:8081';
+export default {
+  data() {
+    return {
+      messages: null,
+    };
+  },
+  async created() {
+    const resp = await fetch(langApiOrigin);
+    this.messages = await resp.json();
+  },
+  methods: {
+    update(locale, key, value) {
+      this.messages[locale][key] = value;
+    },
+    async save() {
+      await fetch(langApiOrigin, {
+        method: 'PUT',
+        body: JSON.stringify(this.messages, null, 2),
+      });
+    },
+  },
+};
+</script>
