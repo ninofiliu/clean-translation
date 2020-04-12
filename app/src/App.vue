8f3139a9062936585cdf3a997fae4ddfd3d8d2a8
ninofiliu <nino.filiu@gmail.com>
2020-04-12 15:48:34 +0200

    add i18n features

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    {{ $t('hello') }}
+    <select v-model="$i18n.locale">
+      <option
+        v-for="{ locale, text } in langs"
+        :key="locale"
+        :value="locale"
+      >
+        {{ text }}
+      </option>
+    </select>
+    <div>
+      {{ $t('name') }}:
+      <input v-model="name">
+      {{ $t('hello', { name }) }}
+    </div>
+    <div>
+      <input v-model="count" type="number">
+      {{ $tc('apples', count) }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      langs: [
+        { locale: 'en', text: 'English' },
+        { locale: 'fr', text: 'French' },
+      ],
+      name: 'John Doe',
+      count: 0,
+    };
+  },
+};
+</script>
